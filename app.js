790cc31b8b8801256094b6b1e1c9ab7ef6e5a251
Saeed Ali <saeedaliofficial77@gmail.com>
2025-10-17 19:10:27 +0500

    hoisting in functions

--- app.js
+++ app.js
@@ -3,6 +3,19 @@
 // Hoisting is a JavaScript mechanism where variables and function declarations are moved to the top of their containing scope during the compile phase before the code is executed. This means that you can use variables and functions before they are declared in the code.
 
 // Example of variable hoisting with var
-console.log(myVar); // Output: undefined
-var myVar = 10;
-console.log(myVar); // Output: 10
+// console.log(myVar); // Output: undefined
+// var myVar = 10;
+// console.log(myVar); // Output: 10
+
+// Example of function hoisting
+
+myFunc()
+
+var myFunc = function() {
+    console.log("Fisrt");
+}
+myFunc();
+function myFunc() {
+    console.log("Second");
+}
+myFunc();
