c1d5f20da355e6ffe88713035a085822e9c904cd
Saeed Ali <saeedaliofficial77@gmail.com>
2025-11-04 09:09:37 -0800

    how we can create custom map method

--- app.js
+++ app.js
@@ -148,3 +148,21 @@
 // })
 
 // console.log(sum);
+
+
+//Polyfills
+
+// Polyfill for map
+
+Array.prototype.myMap = function(callback){
+  const result = [];
+  for(let i=0;i<this.length;i++){
+    result.push(callback(this[i],i,this));
+  }
+  return result;
+} 
+
+const nums = [1,2,3,4,5];
+
+const multiplyByTwo = nums.myMap((num) => num * 2);
+console.log(multiplyByTwo);
