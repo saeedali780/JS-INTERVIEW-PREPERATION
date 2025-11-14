c9c2dbd346a9bb00106fcc5448c611b16c708344
Saeed Ali <saeedaliofficial77@gmail.com>
2025-11-14 13:35:55 -0800

    Variable Shadowing in Js

--- variables.js
+++ variables.js
@@ -1,22 +1,34 @@
 // Var, let and const
-var globalVar = "I am a global variable";
+// var globalVar = "I am a global variable";
 
-function testScope() {
-    var functionVar = "I am a function-scoped variable";
-    let blockLet = "I am a block-scoped variable";
+// function testScope() {
+//     var functionVar = "I am a function-scoped variable";
+//     let blockLet = "I am a block-scoped variable";
 
-    if (true) {
-        let innerBlockLet = "I am an inner block-scoped variable";
-        console.log(innerBlockLet); // Accessible here
+//     if (true) {
+//         let innerBlockLet = "I am an inner block-scoped variable";
+//         console.log(innerBlockLet); // Accessible here
+//     }
+//     console.log(functionVar); // Accessible here
+//     // console.log(innerBlockLet); // Unaccessible here, would throw an error
+// }
+// testScope();
+// console.log(globalVar); // Accessible here
+// // console.log(functionVar); // Unaccessible here, would throw an error
+// // console.log(blockLet); // Unaccessible here, would throw an error
+
+// // Hoisting
+// console.log(hoistedVar); // undefined due to hoisting
+// var hoistedVar = "I am hoisted";
+
+
+// Variable Shadowing
+function test(){
+    let x = "Hello"
+    if(true){
+        let x = "hi"
+        console.log(x);
     }
-    console.log(functionVar); // Accessible here
-    // console.log(innerBlockLet); // Unaccessible here, would throw an error
+console.log(x);  
 }
-testScope();
-console.log(globalVar); // Accessible here
-// console.log(functionVar); // Unaccessible here, would throw an error
-// console.log(blockLet); // Unaccessible here, would throw an error
-
-// Hoisting
-console.log(hoistedVar); // undefined due to hoisting
-var hoistedVar = "I am hoisted";
+test()
