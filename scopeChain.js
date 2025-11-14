291e71fda8e20b4a1405ceb12bf76b33ebe68305
Saeed Ali <saeedaliofficial77@gmail.com>
2025-11-14 07:21:51 -0800

    Scope chaining in JavaScript initial commit

---
+++ scopeChain.js
@@ -0,0 +1,7 @@
+// scopeChain.js
+
+// Scope chaining in JavaScript refers to the hierarchical structure of scopes
+//  that determines how variable names are resolved in nested functions and blocks.
+//  When a variable is accessed, JavaScript first looks for it in the current scope.
+//  If it doesn't find it there, it moves up to the next outer scope, continuing this 
+// process until it either finds the variable or reaches the global scope.
