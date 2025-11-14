9e06d1f8f97f7dd89e898054582f7e0827bf497e
Saeed Ali <saeedaliofficial77@gmail.com>
2025-11-14 07:46:08 -0800

    Scope chaining in JavaScript

--- scopeChain.js
+++ scopeChain.js
@@ -5,3 +5,21 @@
 //  When a variable is accessed, JavaScript first looks for it in the current scope.
 //  If it doesn't find it there, it moves up to the next outer scope, continuing this 
 // process until it either finds the variable or reaches the global scope.
+
+// Example to illustrate scope chaining
+var globalVar = 'I am a global variable';
+function outerFunction() {
+    let outerVar = 'I am from the outer function';
+    function innerFunction() {
+        let innerVar = 'I am from the inner function';
+        console.log(innerVar); // Accessible: 'I am from the inner function'
+        console.log(outerVar); // Accessible: 'I am from the outer function'
+        console.log(globalVar); // Accessible: 'I am a global variable'
+    }
+    innerFunction();
+    // console.log(innerVar); // Unaccessible: ReferenceError
+    console.log(globalVar); // Accessible: 'I am a global variable'
+    
+}
+outerFunction();
+// console.log(outerVar); // Unaccessible: ReferenceError
