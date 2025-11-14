827fd68683b06c3c8d6692c0282861b0ddc1779e
Saeed Ali <saeedaliofficial77@gmail.com>
2025-11-14 07:58:52 -0800

    initial commit

---
+++ variables.js
@@ -0,0 +1,15 @@
+// Var, let and const
+var globalVar = "I am a global variable";
+
+function testScope() {
+    var functionVar = "I am a function-scoped variable";
+    let blockLet = "I am a block-scoped variable";
+
+    if (true) {
+        let innerBlockLet = "I am an inner block-scoped variable";
+        console.log(innerBlockLet); // Accessible here
+    }
+    console.log(functionVar); // Accessible here
+    // console.log(innerBlockLet); // Unaccessible here, would throw an error
+}
+
