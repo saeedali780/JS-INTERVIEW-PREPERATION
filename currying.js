d48250a2280e3db8e3a36f21e96c0c4259fea85f
Saeed Ali <saeedaliofficial77@gmail.com>
2026-06-13 19:59:45 -0400

    //Q2 Evaluate ("Sum")(4)(6) => 10 //   Evaluate ("Multiply")(4)(6) => 24 //   Evaluate ("Divide")(4)(6) => 0.6666666666666666 //   Evaluate ("Subtract")(4)(6) => -2

--- currying.js
+++ currying.js
@@ -16,6 +16,32 @@
 // let logger = sum(1)(2)(3);
 // console.log(logger); // Output: 6
 
-const sum = a => b => c=> a+b+c;
-logger = sum(1)(2)(3);
-console.log(logger); // Output: 6
+// const sum = a => b => c=> a+b+c;
+// logger = sum(1)(2)(3);
+// console.log(logger); // Output: 6
+
+//Q2 Evaluate ("Sum")(4)(6) => 10
+//   Evaluate ("Multiply")(4)(6) => 24
+//   Evaluate ("Divide")(4)(6) => 0.6666666666666666
+//   Evaluate ("Subtract")(4)(6) => -2
+
+function calculator(operation){
+  return function(a){
+    return function(b){
+      if(operation === "Sum"){
+        return a+b;
+      }else if(operation === "Multiply"){
+        return a*b;
+      }else if(operation === "Divide"){
+        return a/b;
+      }else if(operation === "Subtract"){
+        return a-b;
+    }else{
+      return "Invalid Operation"
+    }
+  }
+  }
+}
+
+logger = calculator("Sum")(4)(6);
+console.log(logger); // Output: 10
