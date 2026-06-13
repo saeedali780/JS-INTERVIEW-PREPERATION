a950362a8651202893443068b09f4f4d2409db40
Saeed Ali <saeedaliofficial77@gmail.com>
2026-06-13 19:45:47 -0400

    Q1 how do you implement sum(1)(2)(3) ? (Done)

--- currying.js
+++ currying.js
@@ -3,12 +3,15 @@
 // the next argument. It is a conversion of functions from callable as f(a,b,c)into callable
 // as f(a)(b)(c).
 
-function evaluate(a){
+//Q1 how do you implement sum(1)(2)(3) ?
+
+function sum(a){
   return function(b){
     return function(c){
       return a+b+c;
-    } 
+    }
   }
 }
 
-console.log(evaluate(1)(2)(3)); // 6
+let logger = sum(1)(2)(3);
+console.log(logger); // Output: 6
