00088c6b9fe04a56dab4b64c7312449644f7b984
Saeed Ali <saeedaliofficial77@gmail.com>
2026-06-17 19:25:08 -0400

    delete property is only used with objects it can not delete the local variable since a is a  local variable

--- objects.js
+++ objects.js
@@ -1,7 +1,13 @@
-const obj = {
-    name: 'John',
-    age: 30,
-    city: 'New York'
-}
-console.log(obj.name); // Output: John
-console.log(obj['age']); // Output: 30
+// const obj = {
+//     name: 'John',
+//     age: 30,
+//     city: 'New York'
+// }
+// console.log(obj.name); // Output: John
+// console.log(obj['age']); // Output: 30
+
+const func = (function(a,b) {
+    delete a;
+    return a;
+})("Hello", "World");
+console.log(func); // Output: Hello
