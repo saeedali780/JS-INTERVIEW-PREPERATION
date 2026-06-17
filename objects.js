602122d2c51bc2ab4ec7074c7e7c8c44a365dbf7
Saeed Ali <saeedaliofficial77@gmail.com>
2026-06-17 19:27:54 -0400

    this is how we can add the dynamic key value pairs in an object with space and how to access it.

--- objects.js
+++ objects.js
@@ -6,8 +6,19 @@
 // console.log(obj.name); // Output: John
 // console.log(obj['age']); // Output: 30
 
-const func = (function(a,b) {
-    delete a;
-    return a;
-})("Hello", "World");
-console.log(func); // Output: Hello
+// const func = (function(a,b) {
+//     delete a;
+//     return a;
+// })("Hello", "World");
+// console.log(func); // Output: Hello
+
+const user = {
+    name: 'Alice',
+    age: 25,
+    city: 'Los Angeles',
+    "Hello World": "This is a test",
+}
+
+console.log(user.name); // Output: Alice
+console.log(user['age']); // Output
+console.log(user["Hello World"]); // Output: This is a test`
