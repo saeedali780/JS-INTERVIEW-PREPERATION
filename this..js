32e6b977e401c587f43a70434c018ee7c49c3d47
Saeed Ali <saeedaliofficial77@gmail.com>
2026-06-19 11:56:49 -0400

    intial commit

---
+++ this..js
@@ -0,0 +1,4 @@
+// this keyword in js 
+// this keyword refers to the object that is executing the current function. 
+// It can refer to different objects depending on how the function is called.
+
