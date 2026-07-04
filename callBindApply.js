6599665f03a79f7fb3565a4d30c747d29e4b7468
Saeed Ali <saeedaliofficial77@gmail.com>
2026-07-04 17:33:17 -0400

    what is call in js

---
+++ callBindApply.js
@@ -0,0 +1,6 @@
+// what is call ?
+// The call() method is a built-in JavaScript function that 
+// allows you to invoke a function with a specified this value
+//  and arguments provided individually. 
+// It is used to change the context of the function, 
+// allowing you to call a function as if it were a method of another object.
