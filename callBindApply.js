0009cebe13aacaa27f0611d2f032ef7f4cfc215c
Saeed Ali <saeedaliofficial77@gmail.com>
2026-07-04 18:49:45 -0400

    understood apply method: it takes an array of arguments instead of individual arguments.

--- callBindApply.js
+++ callBindApply.js
@@ -5,13 +5,32 @@
 // It is used to change the context of the function, 
 // allowing you to call a function as if it were a method of another object.
 
+// const obj = {
+//     name: "Saeed",
+//     age: 18,
+// }
+
+// function getName(age) {
+//    return "Hello " + this.name + " Age is " + this.age;
+// }
+
+// console.log(getName.call(obj,20)); // Output: "Hello Saeed Age is 18"
+
+
+// what is apply?
+// The apply() method is similar to the call() method,
+// but it takes an array of arguments instead of individual arguments.
+// It allows you to invoke a function with a specified this value
+// and an array of arguments. The apply() method is useful when you have
+// an array of values that you want to pass as arguments to a function.
+
+
 const obj = {
     name: "Saeed",
-    age: 18,
 }
 
-function getName(age) {
-   return "Hello " + this.name + " Age is " + this.age;
+function getName(age, profession) {
+   return "Hello " + this.name + " Age is " + this.age + " Profession is " + profession;
 }
 
-console.log(getName.call(obj,20)); // Output: "Hello Saeed Age is 18"
+console.log(getName.apply(obj,[20,"Software Engineer"])); // Output: "Hello Saeed Age is 18 Profession is Software Engineer"
