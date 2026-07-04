ae20117975b63c460f011ef7e8a226abca3bf45e
Saeed Ali <saeedaliofficial77@gmail.com>
2026-07-04 17:44:51 -0400

    call concept is cleared: It is used to change the context of the function

--- callBindApply.js
+++ callBindApply.js
@@ -1,6 +1,17 @@
 // what is call ?
 // The call() method is a built-in JavaScript function that 
 // allows you to invoke a function with a specified this value
-//  and arguments provided individually. 
+// and arguments provided individually. 
 // It is used to change the context of the function, 
 // allowing you to call a function as if it were a method of another object.
+
+const obj = {
+    name: "Saeed",
+    age: 18,
+}
+
+function getName(age) {
+   return "Hello " + this.name + " Age is " + this.age;
+}
+
+console.log(getName.call(obj,20)); // Output: "Hello Saeed Age is 18"
