95d81ebdef619056430627765e8e4eb3a9d65c74
Saeed Ali <saeedaliofficial77@gmail.com>
2026-07-20 17:00:58 -0400

    output based question resolved on js executes Synchrounous code first and then the  Asynchronous code

--- promises.js
+++ promises.js
@@ -7,42 +7,12 @@
 
 console.log("Start of the program");
 
-function importAction(username, callback) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(`Hello, ${username} you subscribed to the channel!`);
-        }, 1000);
-    });
-}
+const promise1 = new Promise((res,rej)=>{
+    console.log(1);
+    res(2);
+});
 
-function likeTheVideo(video,cb){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(`You liked the video: ${video}`);
-        }, 1000);
-    });
-}
-function shareTheVideo(video,cb){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(`You shared the video: ${video}`);
-        }, 1000);
-    });
-}
-
-const result = async () => {
-    try{
-    const message1 = await importAction("John Doe");
-    const message2 = await likeTheVideo("JavaScript Promises");
-    const message3 = await shareTheVideo("JavaScript Promises");
-
-    console.log({message1, message2, message3});
-    } catch (error) {
-        console.error("An error occurred:", error);
-    }
-}
-
-result()
-
-console.log("This is end of the program");
-
+promise1.then((value)=>{
+    console.log(value);
+});
+console.log("End of the program");
