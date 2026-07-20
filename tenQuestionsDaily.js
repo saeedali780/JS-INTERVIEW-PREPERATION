393e951bca8b31f7a579d18d33633ee02787fc6b
Saeed Ali <saeedaliofficial77@gmail.com>
2026-07-20 19:58:59 -0400

    Question 1 Resolved

---
+++ tenQuestionsDaily.js
@@ -0,0 +1,14 @@
+// Question 1 — Print Your Information
+// 1. Variables
+
+// Create three variables:
+// name
+// age
+// country
+// Print them in a meaningful sentence.
+
+const name = "Saeed Ali";
+const age = 18;
+const city = "Karachi";
+
+console.log(`My name is ${name}, I am ${age} years old and I live in ${city}.`);
