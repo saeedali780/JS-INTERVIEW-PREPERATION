01ebdd96891a891c25bc934fef375242fa2e40b6
Saeed Ali <saeedaliofficial77@gmail.com>
2026-07-19 20:34:53 -0400

    async await with promises

--- promises.js
+++ promises.js
@@ -18,7 +18,7 @@ function importAction(username, callback) {
 function likeTheVideo(video,cb){
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            reject(`You liked the video: ${video}`);
+            resolve(`You liked the video: ${video}`);
         }, 1000);
     });
 }
@@ -30,14 +30,19 @@ function shareTheVideo(video,cb){
     });
 }
 
-Promise.any([
-    importAction("John"),
-    likeTheVideo("JavaScript Promises"),
-    shareTheVideo("JavaScript Promises")
-]).then((res)=>{
-    console.log(res);
-}).catch((error)=>{
-    console.error("An error occurred:", error);
-});
+const result = async () => {
+    try{
+    const message1 = await importAction("John Doe");
+    const message2 = await likeTheVideo("JavaScript Promises");
+    const message3 = await shareTheVideo("JavaScript Promises");
+
+    console.log({message1, message2, message3});
+    } catch (error) {
+        console.error("An error occurred:", error);
+    }
+}
+
+result()
+
 console.log("This is end of the program");
 
