1c1519985a61b4e603d2ade9a9c6da138e279d78
Saeed Ali <saeedaliofficial77@gmail.com>
2026-07-20 20:01:47 -0400

    Q 2. Even or Odd resolved

--- tenQuestionsDaily.js
+++ tenQuestionsDaily.js
@@ -7,8 +7,25 @@
 // country
 // Print them in a meaningful sentence.
 
-const name = "Saeed Ali";
-const age = 18;
-const city = "Karachi";
+// const name = "Saeed Ali";
+// const age = 18;
+// const city = "Karachi";
 
-console.log(`My name is ${name}, I am ${age} years old and I live in ${city}.`);
+// console.log(`My name is ${name}, I am ${age} years old and I live in ${city}.`);
+
+// Q 2. Even or Odd
+
+// Write a function:
+
+// isEven(num)
+
+// Return true if the number is even, otherwise false.
+
+function isEven(num){
+    if(num % 2 ===0){
+        return "Even: " +  num;
+    }else{
+        return "Odd: " + num;
+    }
+}
+console.log(isEven(4)); // Output: Even: 4
