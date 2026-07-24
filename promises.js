6ea965b6a8d33a510b49ddcca31fadc3b7d82cc8
Saeed Ali <saeedaliofficial77@gmail.com>
2026-07-23 18:20:34 -0700

    started learning pollyfills

--- promises.js
+++ promises.js
@@ -6,15 +6,51 @@
 // promises in js Output based Questions
 
 
-async function loadJson(url){
-    let response = await fetch(url);
-   if (response.status == 200) {
-    let json = await response.json();
-    return json;
+// async function loadJson(url){
+//     let response = await fetch(url);
+//    if (response.status == 200) {
+//     let json = await response.json();
+//     return json;
+//   }
+//   throw new Error(response.status);
+// }
+
+// loadJson('https://jsonplaceholder.typicode.com/todos/1')
+//   .then(json => console.log(json))
+//   .catch(err => console.log(err));
+
+// Promise Pollyfills implementation
+
+function PromisePollyfill(executor){
+  let onResolve,onReject;
+  function resolve(value){
+    onResolve(value)
+  };
+  function reject(value){
+    onReject(value)
   }
-  throw new Error(response.status);
+
+  this.then = function(callback){
+   onResolve = callback
+   return this;
+
+  };
+  this.catch = function(callback){
+    onReject = callback
+    return this
+  };
+
+  executor(resolve, reject)
 }
 
-loadJson('https://jsonplaceholder.typicode.com/todos/1')
-  .then(json => console.log(json))
-  .catch(err => console.log(err));
+const exapmePromise = new PromisePollyfill((resolve,reject)=>{
+  setTimeout(()=>{
+    resolve(2);
+  },1000);
+});
+
+exapmePromise.then((res)=>{
+  console.log(res);
+}).catch((error)=>{
+  console.log(error);
+})
